4b5fe470bea7088d87515343587110dcc2c2ac9a
Sandra <krishnasandra19@gmail.com>
2022-11-04 13:36:25 +0530

    questions

---
+++ questions/queapp/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Questions(models.Model):
+    title=models.CharField(max_length=250)
+    decription=models.CharField(max_length=250)
+    image=models.ImageField(upload_to="images",null=True)
+    created_date=models.DateField(auto_now_add=True)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+class Answers(models.Model):
+    Questions=models.ForeignKey(Questions,on_delete=models.CASCADE)
+    answer=models.CharField(max_length=200)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    upvote=models.ManyToManyField(User,related_name="upvote")
+    created_date=models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.answer
